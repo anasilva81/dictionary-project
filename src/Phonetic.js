4058c31db83ac0d54d54559d9a4aa77eac285252
anasilva81 <anasilva81@users.noreply.github.com>
2022-11-08 22:17:11 +0000

    Create phonetics component

---
+++ src/Phonetic.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Phonetic.css";
+
+export default function Phonetic(props) {
+  let audio = new Audio(props.phonetic.audio);
+
+  const start = () => {
+    audio.play();
+  };
+
+  return (
+    <div className="Phonetic">
+      <button className="btn btn-primary border-0" onClick={start}>
+        ▶︎ &nbsp; Listen
+      </button>
+      <span className="pronounce">{props.phonetic.text}</span>
+    </div>
+  );
+}
